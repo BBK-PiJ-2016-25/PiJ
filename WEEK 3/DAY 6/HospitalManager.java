b2e165a621ed33d14ac55a8f1d7ab7b49ae41936
BBK-PiJ-2016-25 <ahawes02@dcs.bbk.ac.uk>
2016-10-31 17:47:21 +0000

    Added patient and Hospital manager classes

---
+++ WEEK 3/DAY 6/HospitalManager.java
@@ -0,0 +1,43 @@
+
+public class HospitalManager {
+
+	private Patient[] patientArray = new Patient[90];
+
+	public void addPatient(Patient newPatient) {
+		if (firstPatient = null) {
+			firstPatient = newPatient;
+			return;
+		}
+
+		Patient current = firstPatient;
+		while (current.getNextPatient() != null) {
+			current = current.getNextPatient();
+		}
+
+		current.setNextPatient(newPatient);
+	}
+
+	public boolean deletePatient(String name) {
+		
+		if (firstPatient == null) {
+			return false;
+		}
+
+		if (firstPatient.getName().equals(name)) {
+			firstPatient = firstPatient.getNextPatient();
+			return true;
+		}
+
+		Patient current = firstPatient;
+		while (current.getNextPatient() != null) {
+			if (current.getNextPatient().getName().equals(name)) {
+				current.setNextPatient(current.getNextPatient().getNextPatient());
+				return true;
+			}
+			current = current.getNextPatient();
+		}
+
+		return false;
+	}
+
+}
---
+++ WEEK 3/DAY 6/Patient.java
@@ -0,0 +1,36 @@
+
+public class Patient {
+	private String name;
+	private int age;
+	private String illness;
+// methods like constructors, getters
+// and setters come here...
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(name) {
+		this.name = name;
+	}
+
+	public String getIllness() {
+		return illness;
+	}
+
+	public void setIllness(illness) {
+		this.illness = illness;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(age) {
+		this.age = age;
+	}
+
+
+
+}
