689f5c70e587e263bb4c9a0995471e892d8219bc
BBK-PiJ-2016-25 <ahawes02@dcs.bbk.ac.uk>
2016-11-07 20:08:32 +0000

    Added attempted implementation of reverse method- not yet functional

--- WEEK 3/DAY 5/ArrayChecker.java
+++ WEEK 3/DAY 5/ArrayChecker.java
@@ -14,5 +14,28 @@ public class ArrayChecker {
 
 	}
 	
+	public int[] reverse(int[] array) {
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			array[i] = array[array.length-(i+1)];
+			
+		}
+		
+		return array;
+				
+	}
+	
+	public void print(int[] array) {
+		
+		for (int i = 0; i < array.length; i++) {
+		
+		System.out.print(array[i] + " ");
+		
+		}
+	
+	}
+	
+	
 
 }
--- WEEK 3/DAY 5/ArrayCheckerLauncher.java
+++ WEEK 3/DAY 5/ArrayCheckerLauncher.java
@@ -5,16 +5,19 @@ public class ArrayCheckerLauncher {
 
 		ArrayChecker myArrayChecker = new ArrayChecker();
 		
-		int [] array1 = {1,2,3,2,1};
-		int [] array2 = {1,2,3,2,5};
-		int [] array3 = {8,9,9,8};
-		int [] array4 = {8,9,10,11};
+		int[] array1 = {1,2,3,2,1};
+		int[] array2 = {1,2,3,2,5};
+		int[] array3 = {8,9,9,8};
+		int[] array4 = {8,9,10,11};
 		
 		System.out.println(myArrayChecker.isSymmetrical(array1));
 		System.out.println(myArrayChecker.isSymmetrical(array2));
 		System.out.println(myArrayChecker.isSymmetrical(array3));
 		System.out.println(myArrayChecker.isSymmetrical(array4));
-			
+		
+		myArrayChecker.print(myArrayChecker.reverse(array1));
+		myArrayChecker.print(myArrayChecker.reverse(array2));
+		myArrayChecker.print(myArrayChecker.reverse(array3));
+		myArrayChecker.print(myArrayChecker.reverse(array4));
 	}
-
 }
