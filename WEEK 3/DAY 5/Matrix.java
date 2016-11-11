5b46afe055b482ecd025368f7ededc8f84231739
Annie Hawes <annielucyhawes@gmail.com>
2016-11-11 20:50:21 +0000

    Added setElement and setRow methods to Matrix class

--- WEEK 3/DAY 5/Matrix.java
+++ WEEK 3/DAY 5/Matrix.java
@@ -18,7 +18,54 @@ public class Matrix {
 
 	}
 
+	public void setElement(int column, int row, int value) {
+
+		matrix[column][row] = value;
+
+	}
+
+	public void setRow(int row, String values) {
+
+		int[] valuesArray;
+		valuesArray = new int[values.length()];
+		boolean isValue = true;
+		int j = 0;
+
+		for (int i = 0; i == values.length(); i++) {
+
+			if (isValue) {
+
+				valuesArray[j] = Character.getNumericValue(values.charAt(i)); 
+				j++;
+
+			}
+
+			isValue = !isValue;
+
+		}
+
+		for (int k = 0; k == valuesArray.length; k++) {
+
+			matrix[row][k] = valuesArray[k];
+
+		}
+
+	}
 	
-	
+	public void setColumn(int column, String values) {
+
+	}
+
+	public String toString() {
+
+		return "";
+
+	}
+
+	public void prettyPrint() {
+
+
+	}
+
 
 }
