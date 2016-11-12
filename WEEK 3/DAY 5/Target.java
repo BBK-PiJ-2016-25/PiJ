01da1532f47ee8aade9b980741f266be65d128b2
Annie Hawes <annielucyhawes@gmail.com>
2016-11-12 20:43:50 +0000

    Added init method to Target class

--- WEEK 3/DAY 5/Target.java
+++ WEEK 3/DAY 5/Target.java
@@ -23,6 +23,25 @@ public class Target {
 
 	public void init() {
 
+		for (int i = 0; i < target.length; i++) {
+				
+			for (int j = 0; j < target.length; j++) {
+
+				for (int k = 0; k < target.length; k++) {
+
+				target[i][j][k] = 0;
+
+				}
+
+			}
+		}
+
+		int rowToGuess =  (int) Math.abs(target.length+1 * Math.random());
+		int columnToGuess = (int) Math.abs(target.length+1 * Math.random());
+		int layerToGuess = (int) Math.abs(target.length+1 * Math.random());
+
+		target[rowToGuess][columnToGuess][layerToGuess] = 1;
+
 
 	}
 
