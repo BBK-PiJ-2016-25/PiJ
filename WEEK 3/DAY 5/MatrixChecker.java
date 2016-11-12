56136df9ecf21ca3fb9ce1f0ab2b720019b7ceaa
Annie Hawes <annielucyhawes@gmail.com>
2016-11-12 17:08:32 +0000

    Added MatrixChecker class with initial attempts at isSymmetrical methods for 1 and 2 dimensional arrays

---
+++ WEEK 3/DAY 5/MatrixChecker.java
@@ -0,0 +1,37 @@
+public class MatrixChecker {
+
+
+	public boolean isSymmetrical(int[] array) {
+				
+		for (int i = 0; i <= array.length/2; i++) {
+			
+			if (array[i] != array[array.length-(i+1)]) {
+				
+				return false;
+			}
+		}
+		
+		return true;
+
+	}
+
+	public boolean isSymmetrical(int[][] matrix) {
+			
+		for (int i = 0; i < matrix.length; i++) {
+				
+			for (int j = 0; j < matrix[0]length; j++) {
+
+				if(matrix[i][j] != matrix[j][i]) {
+
+					return false;
+				}
+			}
+		}
+
+		return true;
+
+
+	}
+	
+
+}
