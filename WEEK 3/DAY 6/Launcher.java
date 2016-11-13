cbca6bf6db7bc07e4fb64e62fa08e59e568c89e1
BBK-PiJ-25-2-16 <annielucyhawes@gmail.com>
2016-11-13 03:41:54 +0000

    Created new list, created 10 new patients and added them to the list

---
+++ WEEK 3/DAY 6/Launcher.java
@@ -0,0 +1,40 @@
+public class Launcher {
+	
+	public static void main(String[] args) {
+
+		HospitalManager patientList = new HospitalManager(10);
+
+		Patient matthew = new Patient("Matthew", 25, "Conjunctivitis");
+		Patient mark = new Patient("Mark", 31, "Hernia");
+		Patient luke = new Patient("Luke", 46, "Hayfever");
+		Patient john = new Patient("John", 57, "Broken Leg");
+		Patient mary = new Patient("Mary", 27, "Headache");
+		Patient lucy = new Patient("Lucy", 82, "Common Cold");
+		Patient ruth = new Patient("Ruth", 14, "Appendicitis");
+		Patient barbara = new Patient("Barbara", 90, "Arthritis");
+		Patient jeremy = new Patient("Jeremy", 63, "Flu");
+		Patient emily = new Patient("Emily", 5, "Chicken Pox");
+
+		patientList.addPatient(matthew);
+		patientList.addPatient(mark);
+		patientList.addPatient(luke);
+		patientList.addPatient(john);
+		patientList.addPatient(mary);
+		patientList.addPatient(lucy);
+		patientList.addPatient(ruth);
+		patientList.addPatient(barbara);
+		patientList.addPatient(jeremy);
+		patientList.addPatient(emily);
+
+		
+		for (int i =0; i < patientList.getPatientArray().length; i ++) {
+
+			System.out.println(
+				patientList.getPatientArray()[i].getName() + " " +
+				patientList.getPatientArray()[i].getAge() + " " +
+				patientList.getPatientArray()[i].getIllness()
+				);
+
+		}
+	}
+}
