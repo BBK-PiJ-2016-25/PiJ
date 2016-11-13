052fc746f391350374cd1b65f9bc7ca7d3cd569c
BBK-PiJ-25-2-16 <annielucyhawes@gmail.com>
2016-11-13 16:29:38 +0000

    Moved printing code from launcher body to a method in HospitalManager for reuses

---
+++ WEEK 3/DAY 6/Ex 1, Ex 2- Singly Linked Lists/HospitalManager.java
@@ -0,0 +1,69 @@
+
+public class HospitalManager {
+
+	private Patient firstPatient = null;
+
+	public Patient getFirstPatient() {
+		
+		return firstPatient;
+	}
+
+
+	public void addPatient(Patient newPatient) {
+		if (firstPatient == null) {
+			firstPatient = newPatient;
+			return;
+		}
+
+		Patient current = firstPatient;
+		while (current.getNextPatient() != null) {
+			current = current.getNextPatient();
+		}
+
+		current.setNextPatient(newPatient);
+	}
+
+	public boolean deletePatient(String name) {
+		
+		if (firstPatient == null) {
+			return false;
+		}
+
+		if (firstPatient.getName().equals(name)) {
+			firstPatient = firstPatient.getNextPatient();
+			return true;
+		}
+
+		Patient current = firstPatient;
+		while (current.getNextPatient() != null) {
+			if (current.getNextPatient().getName().equals(name)) {
+				current.setNextPatient(current.getNextPatient().getNextPatient());
+				return true;
+			}
+			current = current.getNextPatient();
+		}
+
+		return false;
+	}
+
+	public void print() {
+
+		Patient current = firstPatient;
+
+			do {
+
+				System.out.println(
+
+					current.getName() + " " +
+					current.getAge() + " " +
+					current.getIllness()
+
+					);
+
+				current = current.getNextPatient();
+
+
+			} while (current != null);
+	}
+
+}
