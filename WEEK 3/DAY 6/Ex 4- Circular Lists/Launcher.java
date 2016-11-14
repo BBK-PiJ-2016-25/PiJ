aa5beb0738c214ddc53b347ebfaea275e26dd6c1
BBK-PiJ-25-2-16 <annielucyhawes@gmail.com>
2016-11-14 02:14:26 +0000

    Added folder for exercise 4, copied over patient class, Launcher with list population, CircularList stub

---
+++ WEEK 3/DAY 6/Ex 4- Circular Lists/Launcher.java
@@ -0,0 +1,32 @@
+public class Launcher {
+	
+	public static void main(String[] args) {
+
+		CircularList patientList = new CircularList();
+
+		Patient matthew = new Patient("Matthew", 25, "Conjunctivitis");
+		Patient mark = new Patient("Mark", 31, "Hernia");
+		Patient luke = new Patient("Luke", 46, "Hayfever");
+		Patient john = new Patient("John", 57, "Broken Leg");
+		Patient mary = new Patient("Mary", 27, "Headache");
+		Patient lucy = new Patient("Lucy", 82, "Common Cold");
+		Patient ruth = new Patient("Ruth", 14, "Appendicitis");
+		Patient barbara = new Patient("Barbara", 90, "Arthritis");
+		Patient jeremy = new Patient("Jeremy", 63, "Flu");
+		Patient emily = new Patient("Emily", 5, "Chicken Pox");
+		Patient jo = new Patient("Jo", 28, "Acne");
+
+		patientList.addPatient(matthew);
+		patientList.addPatient(mark);
+		patientList.addPatient(luke);
+		patientList.addPatient(john);
+		patientList.addPatient(mary);
+		patientList.addPatient(lucy);
+		patientList.addPatient(ruth);
+		patientList.addPatient(barbara);
+		patientList.addPatient(jeremy);
+		patientList.addPatient(emily);
+
+
+	}
+}
---
+++ WEEK 3/DAY 6/Ex 4- Circular Lists/Patient.java
@@ -0,0 +1,60 @@
+
+public class Patient {
+
+	private String name;
+	private int age;
+	private String illness;
+	private Patient nextPatient;
+	private Patient lastPatient;
+
+
+	public Patient(String name, int age, String illness) {
+
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIllness() {
+		return illness;
+	}
+
+	public void setIllness(String illness) {
+		this.illness = illness;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Patient getNextPatient() {
+		return nextPatient;
+	}
+
+	public void setNextPatient(Patient nextPatient) {
+		this.nextPatient = nextPatient;
+	}
+
+	public Patient getLastPatient() {
+		return lastPatient;
+	}
+
+	public void setLastPatient(Patient lastPatient) {
+		this.lastPatient = lastPatient;
+	}
+
+
+}
