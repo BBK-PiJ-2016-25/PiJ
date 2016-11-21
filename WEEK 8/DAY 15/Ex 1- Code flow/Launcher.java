810eec0b26576143f26a698081b3a4ee56819334
BBK-PiJ-2016-25 <ahawes02@dcs.bbk.ac.uk>
2016-11-21 14:11:06 +0000

    Added launcher file for checking answers to exercise 1

---
+++ WEEK 8/DAY 15/Ex 1- Code flow/Launcher.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		Launcher launcher = new Launcher();
+		launcher.launch(6);
+	}
+
+	public void launch(int userInput) {
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+		intList.add(4);
+		intList.add(5);
+		intList.add(6);
+		try {
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+		} catch (IndexOutOfBoundsException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
