ef56621efd25fb36bdfbd6b35ff5074205191f0b
BBK-PiJ-2016-25 <ahawes02@dcs.bbk.ac.uk>
2016-11-30 14:41:00 +0000

    Initial compiling implementation of Executor

---
+++ WEEK 9/DAY 18/Ex 3- Responsive UI/ExecutorImpl.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ExecutorImpl implements Executor {
+
+	private ArrayBlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(8);
+
+	
+	public void execute(Runnable command) {
+		
+		queue.add(command);
+
+		while (queue.size() > 0) {
+			try {
+				Thread t = new Thread(queue.take());
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+}
